1d9cd846ea6280e00695ee1ca573ef9693dcb78a
gustavolage7 <gustavolage7@gmail.com>
2023-02-02 17:10:25 -0300

    Primeira página de Departamento, de lista

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Department> Departments = new List<Department>();
+            string name = "Eletronics";
+            int id = 1;
+            Departments.Add(new Department(id, name));
+            name = "Fashion";
+            id = 2;
+            Departments.Add(new Department(id, name));
+
+            return View(Departments);
+        }
+    }
+}
